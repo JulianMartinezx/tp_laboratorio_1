8db5aa154129871a092e40b6180e5bf93f17de9a
JulianMartinezx <julian.martinez.n@gmail.com>
2021-09-25 21:24:47 -0300

    Carga del tp finalizado

--- JulianMartinezTP1/src/PedidoYMuestraDeInformacion.h
+++ JulianMartinezTP1/src/PedidoYMuestraDeInformacion.h
@@ -2,7 +2,22 @@
 #define PEDIDOYMUESTRADEINFORMACION_H_
 #include <stdio.h>
 
+/// @fn void IngresoDeOperandos(float[], int)
+/// @brief Esta es la funcion encargada de solicitar y guardar los valores a ser ingresados en caso de ser presionadas
+/// 	   las opciones 1 o 2 del menu.
+/// @param operandos En este vector (de dos posiciones) se guardan los valores ingresados por el usuario para ser utilizados
+/// 	   mas tarde.
+/// @param posicion Esta variable se encarga de (mediante un if) definir si el valor ingresado se guargada como Primer
+/// 	   operando o como Segundo operando. Decidi trabajar con una funcion escalar (en vez de guardar los valores directamente
+/// 	   en cada Case), para poder realizar esta tarea en una funcion y no hacerlo dentro del menu.
 void IngresoDeOperandos(float operandos[],int posicion);
+
+/// @fn void InformeDeResultados(float[], float[])
+/// @brief Esta es la funcion encargada de mostrarle al usuario los resultados de cada una de las operaciones, segun
+/// 	   los valores que ingreso. En caso de que el usuario ingresara un operador con el que la calculadora no pueda
+/// 	   operar, en vez del resultado mostrara un mensaje de error.
+/// @param operandos Es el vector donde vienen guardados los valores ingresados por el usuario.
+/// @param resultadoOperaciones Es el vector donde vienen cargados los resultados a mostrar de cada operacion.
 void InformeDeResultados(float operandos[],float resultadoOperaciones[]);
 
 #endif /* PEDIDOYMUESTRADEINFORMACION_H_ */
--- JulianMartinezTP1/src/operaciones.h
+++ JulianMartinezTP1/src/operaciones.h
@@ -2,10 +2,39 @@
 #define OPERACIONES_H_
 #include <stdio.h>
 
+/// @fn void SumaDeOperandos(float[], float[])
+/// @brief Funcion encargada de la suma de los valores ingresados
+/// @param operandos Este es el vector (de dos posiciones) donde vienen cargados los valores ingresados por el usuario
+/// @param resultadoOperaciones Este es el vector (de 6 posiciones) donde se guardan los resultados de cada operacion
 void SumaDeOperandos(float operandos[],float resultadoOperaciones[]);
+
+/// @fn void RestaDeOperandos(float[], float[])
+/// @brief Funcion encargada de la resta de los valores ingresados
+/// @param operandos Es el mismo vector (con los valores ingresados por el usuario) que recibe la Funcion Suma
+/// @param resultadoOperaciones Es el mismo vector donde se cargan los resultados de todas las operaciones
+///
 void RestaDeOperandos(float operandos[],float resultadoOperaciones[]);
+/// @fn void DivisionDeOperandos(float[], float[])
+/// @brief Funcion encargada de la division de los valores ingresados, a diferencia
+/// 	   de las demas (y al igual que la funcion Factorial) esta trabaja con una validacion
+/// @param operandos Es el mismo vector (con los valores ingresados por el usuario) que recibe la Funcion Suma
+/// @param resultadoOperaciones Es el mismo vector donde se cargan los resultados de todas las operaciones
+///
 void DivisionDeOperandos(float operandos[],float resultadoOperaciones[]);
+
+/// @fn void MultiplicacionDeOperandos(float[], float[])
+/// @brief Funcion encargada de la multiplicacion de los valores ingresado
+/// @param operandos Es el mismo vector (con los valores ingresados por el usuario) que recibe la Funcion Suma
+/// @param resultadoOperaciones Es el mismo vector donde se cargan los resultados de todas las operaciones
 void MultiplicacionDeOperandos(float operandos[],float resultadoOperaciones[]);
+
+/// @fn int FactorialDeOperandos(int)
+/// @brief La funcion Factorial, factorea los valores ingresados
+/// 	   uno por uno, validandolos en primera instancia para despues realizar la operacion
+/// @param operando En el caso de la funcion Factorial, no recibe al mismo tiempo los dos valores
+/// 	   ingresados por el usuario, sino que los recibe y los opera de a uno mediante la variable Operando, de tipo entero
+/// @return La funcion devuelve a su llamada el valor del numero ingresado tras ser factoreado
+/// 	    y lo hace a traves de la variable ResultadoFactorial
 int FactorialDeOperandos(int operando);
 
 #endif /* OPERACIONES_H_ */
